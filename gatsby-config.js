4bbf44810b053388dc7ee6629b931614e98bbaf1
Cameron Parker <ctrlaltdelete63@gmail.com>
2019-12-12 15:00:33 -0500

    Deleted unneeded files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Thomas Street Church',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This repo houses the Gatsby site for Thomas Street Church.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
